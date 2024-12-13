d4f00bb11619c132590cf27ee911b915f0ace738
Amazing HMAN <gonggong624721@gmail.com>
2024-12-13 12:58:18 -0500

    folder structure

--- backend/src/app/my-route/route.ts
+++ backend/src/app/my-route/route.ts
@@ -1,4 +1,4 @@
-import configPromise from '@payload-config'
+import configPromise from '@/config/payload.config'
 import { getPayload } from 'payload'
 
 export const GET = async () => {
--- backend/src/config/payload.config.ts
+++ backend/src/config/payload.config.ts
@@ -7,8 +7,8 @@ import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
 
-import { Users } from './collections/Users'
-import { Media } from './collections/Media'
+import { Users } from '../collections/Users'
+import { Media } from '../collections/Media'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
